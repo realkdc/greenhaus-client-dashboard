580f8b01d39d69060f12840a03162f202957480e
realkdc <realkdc@users.noreply.github.com>
2025-12-08 22:05:52 -0600

    Add support for GOOGLE_DRIVE_PRIVATE_KEY environment variable
    - Allow using GOOGLE_DRIVE_PRIVATE_KEY as alternative to FIREBASE_PRIVATE_KEY
    - Provides more flexibility for Google Drive authentication configuration

--- src/lib/tools/googleDrive.ts
+++ src/lib/tools/googleDrive.ts
@@ -6,7 +6,7 @@ function getDriveClient() {
   const auth = new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_DRIVE_CLIENT_EMAIL || process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      private_key: (process.env.GOOGLE_DRIVE_PRIVATE_KEY || process.env.FIREBASE_PRIVATE_KEY)?.replace(/\\n/g, "\n"),
     },
     scopes: ["https://www.googleapis.com/auth/drive.readonly"],
   });
